41b9e6687a5fb9aa376a5d1790cc8ff4a3886899
vishu_vishal001 <vv9026472@gmail.com>
2024-08-07 20:22:38 +0530

    Push terraform code files

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "random" {}
+
+resource "random_id" "name_suffix" {
+  byte_length = 4
+}
+
+resource "aws_instance" "ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.prefix_name}-${random_id.name_suffix.hex}"
+    }
+  )
+}
+
+output "instance_id" {
+  value = aws_instance.ec2.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.ec2.public_ip
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the instance"
+  type        = string
+}
+
+variable "ami" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+  default     = "ami-0b72821e2f351e396"
+}
+
+variable "instance_type" {
+  description = "The instance type to use for the instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "prefix_name" {
+  description = "The prefix name for the instance"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+  default = {
+    Environment = "Development"
+    Owner       = "Vishal"
+  }
+}
